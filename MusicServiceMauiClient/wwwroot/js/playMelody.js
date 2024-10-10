1ba81f83fafb94450a757a2bc1472639331911f6
Женя Шабельний <ayneg1212@gmail.com>
2024-10-10 20:45:11 +0300

    Updated base layout for client

--- MusicServiceMauiClient/wwwroot/js/playMelody.js
+++ MusicServiceMauiClient/wwwroot/js/playMelody.js
@@ -1,11 +1,11 @@
 window.playMelody = (address, id) => {
     let url = `https://${address}/api/Melodies/${id}`
-    let player = `<audio autoplay style="display: none" id="Player" type="audio/mp3" src='`
+    let player = `<audio autoplay style="display: none" id="Player" preload="metadata" type="audio/mp3" src='`
         + url + '/\' ></audio>'
 
+    $(".player-control-panel").css('visibility', 'visible')
     $("#audio-container").html(player)
 
     $("#Player").load()
     $("#Player").play()
-    $("#state-pic").attr('src', '/img/pause.png')
 }
